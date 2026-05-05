c579bd2c0ed6fab8c77f8ba5e369afe4f079ec62
Mar José <de.la.taes@gmail.com>
2026-05-04 21:11:01 -0300

    creacción del navbar, según la teoría

---
+++ app.mjs
@@ -0,0 +1,32 @@
+import express from 'express';
+import path from 'path';
+import expressLayouts from 'express-ejs-layouts';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// configurar EJS como motor de plantillas  
+app.set('view engine', 'ejs');
+app.set('views', path.join(import.meta.dirname, 'views'));
+// configurar express-ejs-layouts.
+app.use(expressLayouts);
+app.set('layout', 'layout'); // archivo base de layout
+// configurar la carpeta de archivos estáticos.
+app.use(express.static(path.join(import.meta.dirname, 'public')));  
+
+// Ruta principal
+app.get('/', (req, res) => {
+    res.render('index', { 
+        title: 'Página Principal',
+    navbarlinks: [
+    { text: 'Inicio', href: '/', icon: '/icons/home.svg' },
+    { text: 'Acerca de', href: '/about', icon: '/icons/info.svg' },
+    { text: 'Contacto', href: '/contact', icon: '/icons/contact.svg' }
+]
+     });
+});
+
+// Iniciar el servidor:
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
