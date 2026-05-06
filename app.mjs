a9554f4aaf62f385ccf151d572d2622455149d19
Mar José <de.la.taes@gmail.com>
2026-05-06 10:27:52 -0300

    Ejemplo práctico del nodo del sprint 4

--- app.mjs
+++ app.mjs
@@ -12,18 +12,28 @@ app.set('views', path.join(import.meta.dirname, 'ejs.layout', 'views'));
 app.use(expressLayouts);
 app.set('layout', 'layout'); // archivo base de layout
 // configurar la carpeta de archivos estáticos.
-app.use(express.static(path.join(import.meta.dirname, 'public')));  
+app.use(express.static(path.join(import.meta.dirname, 'public')));
 
-// Ruta principal
-app.get('/', (req, res) => {
-    res.render('index', { 
-        title: 'Página Principal',
-    navbarLinks: [
+const navbarLinks = [
     { text: 'Inicio', href: '/', icon: '/icons/home.svg' },
     { text: 'Acerca de', href: '/about', icon: '/icons/info.svg' },
     { text: 'Contacto', href: '/contact', icon: '/icons/contact.svg' }
-]
-     });
+];
+app.locals.navbarLinks = navbarLinks;
+
+// Ruta principal
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Página Principal'
+    });
+});
+// Ruta para la página "Acerca de" renderiza la vista about.ejs.
+app.get('/about', (req, res) => {
+    res.render('about', { title: 'Acerca de Nosotros'});
+});
+// Ruta para la página "Contacto" renderiza la vista contact.ejs.
+app.get('/contact', (req, res) => {
+    res.render('contact', { title: 'Contáctanos'});
 });
 
 // Iniciar el servidor:
